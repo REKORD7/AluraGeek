77c5fb96a368179de5d53ccdd6be019770a17499
Manuel.Gomez <godm8905@gmail.com>
2024-05-18 21:27:32 -0700

    Act fetch con vercel

--- js/services/product-services.js
+++ js/services/product-services.js
@@ -1,11 +1,15 @@
 const productList = () => {
-    return fetch("http://localhost:3000/products")
+    //prueba con fake api local
+    //return fetch("http://localhost:3000/products")
+    
+    //fake api usando vercel para desplegar en linea
+    return fetch("https://fake-api-gold-seven.vercel.app/products")
     .then((res) => res.json())
     .catch((err) => console.log(err));
 };
 
 const createProduct = (name, price, image) => {
-    return fetch("http://localhost:3000/products", {
+    return fetch("https://fake-api-gold-seven.vercel.app/products", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, price, image })
@@ -15,7 +19,7 @@ const createProduct = (name, price, image) => {
 }
 
 const deleteProduct = (id) =>{
-    return fetch(`http://localhost:3000/products/${id}`, {
+    return fetch(`https://fake-api-gold-seven.vercel.app/products/${id}`, {
         method: "DELETE",
     })
     .then((res) => res.json())
